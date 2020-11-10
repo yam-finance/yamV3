a3175f556b7d4eec2c15242684b509748092f544
nate welch <nathanielawelch@gmail.com>
2020-11-10 13:49:15 -0500

    Added more tests for VestingPool.

--- contracts/tests/vesting_pool/VestingPool.sol
+++ contracts/tests/vesting_pool/VestingPool.sol
@@ -14,7 +14,7 @@ contract VestingPool {
         uint256 totalAmount;
         uint256 amountPaidOut;
     }
-    
+
     /**
      * @notice Governor for this contract
      */
@@ -44,8 +44,8 @@ contract VestingPool {
      * @notice Event emitted when a sub gov is enabled/disabled
      */
     event SubGovModified(
-        address account,
-        bool isMod
+        address account, 
+        bool isSubGov
     );
 
     /**
@@ -61,9 +61,7 @@ contract VestingPool {
     /**
      * @notice Event emitted when stream is closed
      */
-    event StreamClosed(
-        uint256 indexed streamId
-    );
+    event StreamClosed(uint256 indexed streamId);
 
     /**
      * @notice Event emitted on payout
@@ -78,7 +76,7 @@ contract VestingPool {
      * @notice Event emitted when pendingGov is changed
      */
     event NewPendingGov(
-        address oldPendingGov,
+        address oldPendingGov, 
         address newPendingGov
     );
 
@@ -86,17 +84,19 @@ contract VestingPool {
      * @notice Event emitted when gov is changed
      */
     event NewGov(
-        address oldGov,
+        address oldGov, 
         address newGov
     );
 
-    constructor(YAMDelegate2 _yam) public {
+    constructor(YAMDelegate2 _yam)
+        public
+    {
         gov = msg.sender;
         yam = _yam;
     }
 
     modifier onlyGov() {
-        require(msg.sender == gov, "VestingPool::onlyGov: account is not owner");
+        require(msg.sender == gov, "VestingPool::onlyGov: account is not gov");
         _;
     }
 
@@ -109,120 +109,8 @@ contract VestingPool {
     }
 
     /**
-     * @dev Opens a new stream that continuously pays out.
-     * @param recipient Account that will receive the funds.
-     * @param length The amount of time in seconds that the stream lasts
-     * @param totalAmount The total amount to payout in the stream
-     */
-    function openStream(
-        address recipient,
-        uint128 length,
-        uint256 totalAmount
-    ) 
-        public
-        canManageStreams
-        returns (uint256 streamIndex)
-    {
-        require(
-            totalUnclaimedInStreams.add(totalAmount) <=
-                yam.balanceOfUnderlying(address(this))
-        );
-        require(length > 0);
-        streamIndex = streamCount++;
-        streams[streamIndex] = Stream({
-            recipient: recipient,
-            length: length,
-            startTime: uint128(block.timestamp),
-            totalAmount: totalAmount,
-            amountPaidOut: 0
-        });
-        totalUnclaimedInStreams = totalUnclaimedInStreams.add(totalAmount);
-        emit StreamOpened(recipient, streamIndex, length, totalAmount);
-    }
-
-    /**
-     * @dev Closes the specified stream. Pays out pending amounts, clears out the stream, and emits a StreamClosed event.
-     * @param streamId The id of the stream to close.
-     */
-    function closeStream(uint256 streamId)
-        public
-        canManageStreams
-    {
-        payout(streamId);
-        streams[streamId] = Stream(
-            address(0x0000000000000000000000000000000000000000),
-            0,
-            0,
-            0,
-            0
-        );
-        emit StreamClosed(streamId);
-    }
-
-   /**
-     * @dev Pays out pending amount in a st ream
-     * @param streamId The id of the stream to payout.
-     * @return The amount paid out in underlying
-     */
-    function payout(uint256 streamId)
-        public
-        returns (uint256 paidOut)
-    {
-        uint128 currentTime = uint128(block.timestamp);
-        Stream memory stream = streams[streamId];
-        require(
-            stream.startTime <= currentTime,
-            "VestingPool::payout: Stream hasn't started yet"
-        );
-        uint256 claimableUnderlying = _claimable(stream);
-
-        streams[streamId].amountPaidOut = stream.amountPaidOut.add(
-            claimableUnderlying
-        );
-
-        totalUnclaimedInStreams = totalUnclaimedInStreams.sub(
-            claimableUnderlying
-        );
-
-        yam.transferUnderlying(stream.recipient, claimableUnderlying);
-
-        emit Payout(streamId, stream.recipient, claimableUnderlying);
-    }
-
-   /**
-     * @dev The amount that is claimable for a stream
-     * @param streamId The stream to get the claimabout amount for.
-     * @return The amount that is claimable for this stream
-     */
-    function claimable(uint256 streamId)
-        external
-        view
-        returns (uint256 claimableUnderlying)
-    {
-        Stream memory stream = streams[streamId];
-        return _claimable(stream);
-    }
-
-    function _claimable(Stream memory stream)
-        internal
-        view
-        returns (uint256 claimableUnderlying)
-    {
-        uint128 currentTime = uint128(block.timestamp);
-        uint128 elapsedTime = currentTime - stream.startTime;
-        if (currentTime >= stream.startTime + stream.length) {
-            claimableUnderlying = stream.totalAmount - stream.amountPaidOut;
-        } else {
-            claimableUnderlying = elapsedTime
-                .mul(stream.totalAmount)
-                .div(stream.length)
-                .sub(stream.amountPaidOut);
-        }
-    }
-
-   /**
      * @dev Set whether an account can open/close streams. Only callable by the current gov contract
-     * @param account The acount to set permissions for.
+     * @param account The account to set permissions for.
      * @param _isSubGov Whether or not this account can manage streams
      */
     function setSubGov(address account, bool _isSubGov)
@@ -256,6 +144,118 @@ contract VestingPool {
         gov = pendingGov;
         pendingGov = address(0);
         emit NewGov(oldGov, gov);
+    }
+
+    /**
+     * @dev Opens a new stream that continuously pays out.
+     * @param recipient Account that will receive the funds.
+     * @param length The amount of time in seconds that the stream lasts
+     * @param totalAmount The total amount to payout in the stream
+     */
+    function openStream(
+        address recipient,
+        uint128 length,
+        uint256 totalAmount
+    )
+        public
+        canManageStreams
+        returns (uint256 streamIndex)
+    {
+        streamIndex = streamCount++;
+        streams[streamIndex] = Stream({
+            recipient: recipient,
+            length: length,
+            startTime: uint128(block.timestamp),
+            totalAmount: totalAmount,
+            amountPaidOut: 0
+        });
+        totalUnclaimedInStreams = totalUnclaimedInStreams.add(totalAmount);
+        require(
+            totalUnclaimedInStreams <= yam.balanceOfUnderlying(address(this)),
+            "VestingPool::payout: Total streaming is greater than pool's YAM balance"
+        );
+        emit StreamOpened(recipient, streamIndex, length, totalAmount);
+    }
+
+    /**
+     * @dev Closes the specified stream. Pays out pending amounts, clears out the stream, and emits a StreamClosed event.
+     * @param streamId The id of the stream to close.
+     */
+    function closeStream(uint256 streamId)
+        public
+        canManageStreams
+    {
+        uint256 amountPaid = payout(streamId);
+        streams[streamId] = Stream(
+            address(0x0000000000000000000000000000000000000000),
+            0,
+            0,
+            0,
+            0
+        );
+        emit StreamClosed(streamId);
+    }
+
+    /**
+     * @dev Pays out pending amount in a stream
+     * @param streamId The id of the stream to payout.
+     * @return The amount paid out in underlying
+     */
+    function payout(uint256 streamId)
+        public
+        returns (uint256 paidOut)
+    {
+        uint128 currentTime = uint128(block.timestamp);
+        Stream memory stream = streams[streamId];
+        require(
+            stream.startTime <= currentTime,
+            "VestingPool::payout: Stream hasn't started yet"
+        );
+        uint256 claimableUnderlying = _claimable(stream);
+        streams[streamId].amountPaidOut = stream.amountPaidOut.add(
+            claimableUnderlying
+        );
+
+        totalUnclaimedInStreams = totalUnclaimedInStreams.sub(
+            claimableUnderlying
+        );
+
+        yam.transferUnderlying(stream.recipient, claimableUnderlying);
+
+        emit Payout(streamId, stream.recipient, claimableUnderlying);
+        return claimableUnderlying;
+    }
+
+
+    /**
+     * @dev The amount that is claimable for a stream
+     * @param streamId The stream to get the claimabout amount for.
+     * @return The amount that is claimable for this stream
+     */
+    function claimable(uint256 streamId)
+        external
+        view
+        returns (uint256 claimableUnderlying)
+    {
+        Stream memory stream = streams[streamId];
+        return _claimable(stream);
+    }
+
+    function _claimable(Stream memory stream)
+        internal
+        view
+        returns (uint256 claimableUnderlying)
+    {
+        uint128 currentTime = uint128(block.timestamp);
+        uint128 elapsedTime = currentTime - stream.startTime;
+        if (currentTime >= stream.startTime + stream.length) {
+            claimableUnderlying = stream.totalAmount - stream.amountPaidOut;
+        } else {
+            claimableUnderlying = elapsedTime
+                .mul(stream.totalAmount)
+                .div(stream.length)
+                .sub(stream.amountPaidOut);
+        }
     }
 
 }
