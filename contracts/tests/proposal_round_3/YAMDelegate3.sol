543daac7b88c01a370f4ea7eca5bc91e27d3e109
nate welch <nathanielawelch@gmail.com>
2020-11-13 23:02:50 -0500

    Updated proposal_round_3.

---
+++ contracts/tests/proposal_round_3/YAMDelegate3.sol
@@ -0,0 +1,88 @@
+pragma solidity 0.5.15;
+
+/* Copyright 2020 Compound Labs, Inc.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+
+import "./YAMLogic3.sol";
+
+contract YAMDelegationStorage {
+    /**
+     * @notice Implementation address for this contract
+     */
+    address public implementation;
+}
+
+contract YAMDelegatorInterface is YAMDelegationStorage {
+    /**
+     * @notice Emitted when implementation is changed
+     */
+    event NewImplementation(address oldImplementation, address newImplementation);
+
+    /**
+     * @notice Called by the gov to update the implementation of the delegator
+     * @param implementation_ The address of the new implementation for delegation
+     * @param allowResign Flag to indicate whether to call _resignImplementation on the old implementation
+     * @param becomeImplementationData The encoded bytes data to be passed to _becomeImplementation
+     */
+    function _setImplementation(address implementation_, bool allowResign, bytes memory becomeImplementationData) public;
+}
+
+contract YAMDelegateInterface is YAMDelegationStorage {
+    /**
+     * @notice Called by the delegator on a delegate to initialize it for duty
+     * @dev Should revert if any issues arise which make it unfit for delegation
+     * @param data The encoded bytes data for any initialization
+     */
+    function _becomeImplementation(bytes memory data) public;
+
+    /**
+     * @notice Called by the delegator on a delegate to forfeit its responsibility
+     */
+    function _resignImplementation() public;
+}
+
+
+contract YAMDelegate3 is YAMLogic3, YAMDelegateInterface {
+    /**
+     * @notice Construct an empty delegate
+     */
+    constructor() public {}
+
+    /**
+     * @notice Called by the delegator on a delegate to initialize it for duty
+     * @param data The encoded bytes data for any initialization
+     */
+    function _becomeImplementation(bytes memory data) public {
+        // Shh -- currently unused
+        data;
+
+        // Shh -- we don't ever want this hook to be marked pure
+        if (false) {
+            implementation = address(0);
+        }
+
+        require(msg.sender == gov, "only the gov may call _becomeImplementation");
+    }
+
+    /**
+     * @notice Called by the delegator on a delegate to forfeit its responsibility
+     */
+    function _resignImplementation() public {
+        // Shh -- we don't ever want this hook to be marked pure
+        if (false) {
+            implementation = address(0);
+        }
+
+        require(msg.sender == gov, "only the gov may call _resignImplementation");
+    }
+}
--- contracts/tests/proposal_round_3/proposal.t.sol
+++ contracts/tests/proposal_round_3/proposal.t.sol
@@ -7,6 +7,8 @@ import "../test_tests/base.t.sol";
 import { DualGovernorAlpha } from "../proposal_round_2/YAMGovernorAlphaWithLps.sol";
 import { MonthlyAllowance } from "../contributor_monthly_payments/MonthlyAllowance.sol";
 import { VestingPool } from "../vesting_pool/VestingPool.sol";
+import { IERC20 } from "../../lib/IERC20.sol";
+import { Timelock } from "../../governance/TimeLock.sol";
 
 contract Prop3 is YAMv3Test {
 
@@ -15,52 +17,65 @@ contract Prop3 is YAMv3Test {
     DualGovernorAlpha contributor_gov = DualGovernorAlpha(0xDceC4A3aA84f79249c1b5325a06c1560d202Dd87);
     MonthlyAllowance monthlyAllowance = MonthlyAllowance(0x03A882495Bc616D3a1508211312765904Fb062d1);
     VestingPool vestingPool = VestingPool(0xDCf613db29E4d0B35e7e15e93BF6cc6315eB0b82);
+    address yamLogic3 = 0x405d5F5b76c94ebc26A28E56961c63cd9E743Af2;
 
     function setUp() public {
         setUpCore();
         yamhelper.becomeAdmin(address(timelock),0xEDf7C3D4CB2e89506C1469709073025d09D47bDd);
     }
 
-
+    event TEST(address yam);
 
     function test_onchain_prop_3() public {
         assertTrue(false);
-        address[] memory targets = new address[](4);
-        uint256[] memory values = new uint256[](4);
-        string[] memory signatures = new string[](4);
-        bytes[] memory calldatas = new bytes[](4);
+        address[] memory targets = new address[](6);
+        uint256[] memory values = new uint256[](6);
+        string[] memory signatures = new string[](6);
+        bytes[] memory calldatas = new bytes[](6);
         string memory description = "Accept admin for new governor, set reserves allowance for contributor allowance, add contributor gov as subgov on contributor allowance";
 
-        // -- Main governor accept admin on contributor governor
+        // -- Accept admin on contributor governor (for taking control of Timelock)
         targets[0] = address(contributor_gov);
         values[0] = 0;
         signatures[0] = "__acceptAdmin()";
         calldatas[0] = "";
 
-        // -- Main governor set approval for monthly allowance to use yUSD
-        targets[1] = address(reserves);
+        // -- Accept gov over vesting pool
+        targets[1] = address(vestingPool);
         values[1] = 0;
-        signatures[1] = "whitelistWithdrawals(address[],uint256[],address[])";
-        address[] memory whos = new address[](1);
-        whos[0] = address(monthlyAllowance);
-        uint256[] memory amounts = new uint256[](1);
-        amounts[0] = uint256(-1);
-        address[] memory tokens = new address[](1);
-        tokens[0] = address(0x5dbcF33D8c2E976c6b560249878e6F1491Bca25c);
-        calldatas[1] = abi.encode(whos, amounts, tokens);
-
+        signatures[1] = "_acceptGov()";
+        calldatas[1] = "";
+        
         // -- Accept gov over monthlyAllowance
         targets[2] = address(monthlyAllowance);
         values[2] = 0;
         signatures[2] = "acceptGov()";
         calldatas[2] = ""; 
 
-        // -- Accept gov for vestingPool
-        targets[3] = address(vestingPool);
+        // -- Set approval on reserves for monthly allowance contract to use yUSD
+        targets[3] = address(reserves);
         values[3] = 0;
-        signatures[3] = "_acceptGov()";
-        calldatas[3] = "";
+        signatures[3] = "whitelistWithdrawals(address[],uint256[],address[])";
+        address[] memory whos = new address[](1);
+        whos[0] = address(monthlyAllowance);
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = uint256(-1);
+        address[] memory tokens = new address[](1);
+        tokens[0] = address(0x5dbcF33D8c2E976c6b560249878e6F1491Bca25c);
+        calldatas[3] = abi.encode(whos, amounts, tokens);
 
+        // -- Set yam imlementation
+        targets[4] = address(yamV3);
+        values[4] = 0;
+        signatures[4] = "_setImplementation(address,bool,bytes)";
+        calldatas[4] = abi.encode(yamLogic3,false,"");
+        
+        // -- Mint YAM to vesting pool
+        targets[5] = address(yamV3);
+        values[5] = 0;
+        signatures[5] = "mintUnderlying(address,uint256)";
+        calldatas[5] = abi.encode(address(vestingPool),100000 * (10**24));
+        
         yamhelper.getQuorum(yamV3, me);
         yamhelper.bing();
 
@@ -72,13 +87,24 @@ contract Prop3 is YAMv3Test {
             description
         );
         
-        // -- Assert that main governor got guardian of contributor governor
-        assertEq(address(timelock), contributor_gov.guardian());
+        // -- Assert contributor timelock had admin set properly
+        address payable wallet = address(uint160(address(contributor_gov.timelock())));
+        assertEq(address(contributor_gov), Timelock(wallet).admin());
         
-        // -- Assert contributor governor timelock is sub gov on monthly allowance contract
-        assertTrue(monthlyAllowance.isSubGov(address(contributor_gov.timelock())));
+        // -- Assert governor timelock for vestingPool
+        assertEq(vestingPool.gov(), address(governor.timelock()));
 
-        // -- Assert contributor governor timelock is sub gov on vesting pool contract
-        assertTrue(vestingPool.isSubGov(address(contributor_gov.timelock())));
+        // -- Assert governor timelock for monthlyAllowance
+        assertEq(monthlyAllowance.gov(), address(governor.timelock()));
+        
+        // -- Assert allowance set for monthlyAllowance
+        assertEq(IERC20(0x5dbcF33D8c2E976c6b560249878e6F1491Bca25c).allowance(address(reserves), address(monthlyAllowance)), uint256(-1));
+
+        // -- Assert implementation set for yam
+        assertEq(yamV3.implementation(), yamLogic3);
+
+        // -- Assert that the vesting pool got the correct amount of YAM
+        assertEq(yamV3.balanceOfUnderlying(address(vestingPool)), 100000 * (10**24));
+
     }
 }
