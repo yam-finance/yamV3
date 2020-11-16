f0923bd989e746ec676506d88cc956ef2cfdde8d
Brock Elmore <brock.elmore@gmail.com>
2020-11-16 14:48:00 -0500

    fixing messed up commit order - add DPI to base.t.sol

--- contracts/tests/test_tests/base.t.sol
+++ contracts/tests/test_tests/base.t.sol
@@ -98,7 +98,7 @@ contract YAMv3Test is DSTest {
     // --- tokens
     address public constant yyCRV = address(0x5dbcF33D8c2E976c6b560249878e6F1491Bca25c);
     address public constant WETH = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-
+    address public constant DPI = address(0x1494CA1F11D487c2bBe4543E90080AeBa4BA3C2b);
     // --- other
     address public constant multiSig = address(0x0114ee2238327A1D12c2CeB42921EFe314CBa6E6);
     address public constant gitcoinGrants = address(0xde21F729137C5Af1b01d73aF1dC21eFfa2B8a0d6);
