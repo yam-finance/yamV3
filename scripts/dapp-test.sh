3709a1c1a86cfa3208e1d47b31680b814c02724b
Scott Moses Sunarto <github@smsunarto.com>
2021-06-30 01:25:40 +0700

    Take test name as an argument

--- scripts/dapp-test.sh
+++ scripts/dapp-test.sh
@@ -6,6 +6,13 @@ oops() {
     exit 1
 }
 
+if [ -z "$1" ]
+  then
+    echo "Missing test name argument"
+    printf "Correct usage: ./scripts/dapp-test.sh [test_name]\n\n"
+    exit 1
+fi
+
 export ETH_RPC_URL="https://fee7372b6e224441b747bf1fde15b2bd.eth.rpc.rivet.cloud/"
 
 export DAPP_SRC=./contracts
@@ -22,4 +29,4 @@ export DAPP_TEST_ORIGIN="0x683A78bA1f6b25E29fbBC9Cd1BFA29A51520De84"
 export DAPP_TEST_CHAINED=99
 export DAPP_TEST_ADDRESS="0x683A78bA1f6b25E29fbBC9Cd1BFA29A51520De84"
 printf 'Running test for address %s\n' "$DAPP_TEST_ADDRESS"
-LANG=C.UTF-8 dapp test --rpc-url "https://fee7372b6e224441b747bf1fde15b2bd.eth.rpc.rivet.cloud/" -v --match test_onchain_prop_12
+LANG=C.UTF-8 dapp test --rpc-url "https://fee7372b6e224441b747bf1fde15b2bd.eth.rpc.rivet.cloud/" -v --match "$1"
